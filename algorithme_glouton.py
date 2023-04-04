c906a93869837afc7335a573da096e98b208cdd4
Thomas Croizet <t.croizet77@gmail.com>
2023-04-04 17:54:34 +0200

    Fix naming

--- algorithme_glouton.py
+++ algorithme_glouton.py
@@ -13,7 +13,7 @@ def aire(p1,p2,p3):
     """
     return abs((p2[0]-p1[0])*(p3[1]-p1[1]) - (p3[0]-p1[0])*(p2[1]-p1[1])) / 2
 
-def triangulation_glouton(poly):
+def triangulation(poly):
     triangles = []
 
     while len(poly.getAllArcs()) > 3: #tant que le polygone n'est pas un triangle
